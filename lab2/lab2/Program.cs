aeb299b174ad94c180e8305847535e6128a88144
Ekaterina <astapkina.caterina@gmail.com>
2020-09-26 16:03:42 +0300

    Lab 2

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Определите переменные всех возможных примитивных типов С# и проинициализируйте их. 
+            //Осуществите ввод и вывод их значений используя консоль.*/
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
