851a21f0c77e0df92910040aaf97a643b9312dd1
Ekaterina <astapkina.caterina@gmail.com>
2020-09-26 16:20:14 +0300

    solution

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -9,7 +9,20 @@ namespace lab2
             /*Определите переменные всех возможных примитивных типов С# и проинициализируйте их. 
             //Осуществите ввод и вывод их значений используя консоль.*/
 
-            Console.WriteLine("Hello World!");
+            bool bl = true;
+            byte bt = 250;
+            sbyte sb = 101;
+            char ch = 'g';
+            decimal dc = 2.1m;
+            double dl = 95.159;
+            float fl = 46.3F;
+            int a = 23;
+            uint ui = 3598;
+            long l = 58793;
+            ulong ul = 468931687621;
+            short sh = 39;
+            ushort ush = 60358;
+
         }
     }
 }
