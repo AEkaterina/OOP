a50f5d65a8d75110100d48b8d9a36d7a18098bd3
Ekaterina <astapkina.caterina@gmail.com>
2020-10-02 18:12:33 +0300

    add the 2 task

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Text;
+
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.IO;
 
 namespace lab2
 {
@@ -6,63 +12,121 @@ namespace lab2
     {
         static void Main(string[] args)
         {
-            /*1a Определите переменные всех возможных примитивных типов С# и проинициализируйте их. 
-            Осуществите ввод и вывод их значений используя консоль.*/
 
-            bool bl = true;
-            byte bt = 250;
-            sbyte sb = 101;
-            char ch = 'g';
-            decimal dc = 2.1m;
-            double dl = 95.159;
-            float fl = 46.3F;
-            int ii = 23;
-            uint ui = 3598;
-            long l = 58793;
-            ulong ul = 468931687621;
-            short sh = 39;
-            ushort ush = 60358;
-            Console.WriteLine("bool = {0}, byte = {1}, sbyte = {2}, char = {3}", bl, bt, sb, ch);
-            Console.WriteLine("decimal = {0}, double = {1}, float = {2}", dc, dl, fl);
-            Console.WriteLine("int = {0}, uint = {1}, long = {2}, ulong = {3}, short = {4}, ushort = {5}", ii, ui, l, ul, sh, ush);
+            ///*1a Определите переменные всех возможных примитивных типов С# и проинициализируйте их. 
+            //Осуществите ввод и вывод их значений используя консоль.*/
 
-            //1b Выполните 5 операций явного и 5 неявного приведения.
+            //bool bl = true;
+            //byte bt = 250;
+            //sbyte sb = 101;
+            //char ch = 'g';
+            //decimal dc = 2.1m;           
+            //float fl = 46.3F;
+            //Console.WriteLine("bool = {0}, byte = {1}, sbyte = {2}, char = {3}", bl, bt, sb, ch);
+            //Console.WriteLine("decimal = {0}, float = {1}", dc, fl);
+            //short sh = 354;
+            //ushort ush = 60358;
+            //uint ui = 3598;
+            //long l = 58793;
+            //ulong ul = 468931687621;
+            //Console.WriteLine("short = {0}, ushort = {1} uint = {2}, long = {3}, ulong = {4}", sh, ush, ui, l, ul);
+            //Console.Write("Input double: ");            
+            //double dl = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Input int: ");
+            //int ii = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"double = {dl}, int = {ii} ");            
+
+            ////1b Выполните 5 операций явного и 5 неявного приведения.
             
-            //неявное
-            int i = 135487;
-            long il = i;
-            short s = 533;
-            int si = s;
-            float f = 8.93F;
-            double fd = f;
-            int ifl = 697;
-            float fli = ifl;
-            long ll = 798451;
-            double ld = l;
+            ////неявное
+            //int i = 135487;
+            //long il = i;
+            //short s = 533;
+            //int si = s;
+            //float f = 8.93F;
+            //double fd = f;
+            //int ifl = 697;
+            //float fli = ifl;
+            //long ll = 798451;
+            //double ld = ll;
 
-            //явное
-            double dd = 896.16447;
-            int id = (int)dd;
-            float ff = 8.9F;
-            short sf = (short)ff;
-            double ddl = 648.568798;
-            long dll = (long)ddl;
-            float ffl = 89.34F;
-            int iifl = (int)ffl;
-            float fbyte = 78.45F;
-            byte bfl = (byte)(fbyte);
+            ////явное
+            //double dd = 896.16447;
+            //int id = (int)dd;
+            //float ff = 8.9F;
+            //short sf = (short)ff;
+            //double ddl = 648.568798;
+            //long dll = (long)ddl;
+            //float ffl = 89.34F;
+            //int iifl = (int)ffl;
+            //float fbyte = 78.45F;
+            //byte bfl = (byte)(fbyte);
 
-            //1c Выполните упаковку и распаковку значимых типов. 
-            Object o = ii;      //упаковка
-            int x = (int)o;     //распаковка
+            ////1c Выполните упаковку и распаковку значимых типов. 
+            //Object o = ii;      //упаковка
+            //int x = (int)o;     //распаковка
 
-            //1d Продемонстрируйте работу с неявно типизированной переменной
-            var str = "Hello";
-            Console.WriteLine(str);
+            ////1d Продемонстрируйте работу с неявно типизированной переменной
+            //var str = "Hello";
+            //Console.WriteLine(str);
 
-            //1e Продемонстрируйте пример работы с Nullable переменной
-            Nullable<int> x1 = 324;
-            Console.WriteLine(x1 + "\n___________________________________________________\n\n");
+            ////1e Продемонстрируйте пример работы с Nullable переменной
+            //Nullable<int> x1 = 65498;
+            //Console.WriteLine(x1);
+
+
+
+            //2a Объявите строковые литералы. Сравните их.
+            string str1 = "cat";
+            string str2 = "kitten";
+            int comp = String.Compare(str1, str2);
+            if (comp == 0)
+                Console.WriteLine("Строки {0} и {1} одинаковы", str1, str2);
+            else
+                Console.WriteLine("строки {0} и {1} различны", str1, str2);
+
+            /*2b Создайте три строки на основе String. Выполните: сцепление, копирование, выделение подстроки, 
+              разделение строки на слова, вставки подстроки в заданную позицию, удаление заданной подстроки. */
+            string s1 = "Astapkina ";
+            string s2 = "Ekaterina";
+            string s3 = "Hello and goodbye";
+            string s4;
+            Console.WriteLine(s1 + s2);    //сцепление 
+            s4 = s3;                        //копирование
+            Console.WriteLine($"Строка \"{s3}\" скопирована в строку s4");
+            Console.WriteLine("Выделение из строки слова " + s4.Substring(10));        //выделение подстроки
+            string[] words = s3.Split(' ');                 //разделение на слова
+            Console.WriteLine($"Разделим строку \"{s3}\" на слова:");
+            foreach (string s in words)
+                Console.WriteLine(s);
+            string news1 = "Hello  and goodbye";                 //вставка подстроки
+            string news2 = news1.Insert(6, s2);
+            Console.WriteLine(news2);
+            Console.WriteLine($"Удаление подстроки в строке \"{news2}\": " + news2.Remove(15));         //удаление подстроки
+
+            /*2c Создайте пустую и null строку. Продемонстрируйте использование метода string.IsNullOrEmpty.Продемонстрируйте
+            что еще можно выполнить с такими строками*/
+            string emptstr = "";
+            string nullstr = null;
+            if (String.IsNullOrEmpty(emptstr))
+                Console.WriteLine("Строка emptstr пустая или null");
+            else
+                Console.WriteLine("Cтрока не null");
+            
+            if (String.IsNullOrEmpty(nullstr))
+                Console.WriteLine("Строка nullstr пустая или null");
+            else
+                Console.WriteLine("Cтрока не null");
+
+            /*2dСоздайте строку на основе StringBuilder. Удалите определенные позиции и добавьте новые символы в начало
+             и конец строки*/            
+            StringBuilder sb = new StringBuilder(" строка моя");
+            sb.Remove(8, 3);            ///удалите опр. позиции
+            sb.Insert(0, "Это");      ///добавить символы в начало
+            sb.Append("новая");       ///и конец строки
+            Console.WriteLine(sb);
+
+
         }
     }
 }
