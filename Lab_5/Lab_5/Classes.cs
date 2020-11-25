3dafb4f57d850f0841844c80fe8e8c515c7648fa
Екатерина Астапкина <astapkina.caterina@gmail.com>
2020-11-26 00:00:10 +0300

    start

---
+++ Lab_5/Lab_5/Classes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class Figure
+    {
+
+    }
+
+    class Circle : Figure
+    {
+
+    }
+
+    class Rectangular : Figure
+    {
+
+    }
+
+    class Element
+    {
+
+    }
+
+    class CheckBox : Element
+    {
+
+    }
+
+    class RadioButton : Element
+    {
+
+    }
+
+    class Button : Element
+    {
+
+    }
+}
